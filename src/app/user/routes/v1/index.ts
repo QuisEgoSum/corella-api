66f926a742a4272a85fed450f0a81799043773b6
QuisEgoSum <subbotin.evdokim@gmail.com>
2021-12-13 23:14:47 +0300

    feat: Added module loaders for load routes in dir

--- src/app/user/routes/v1/index.ts
+++ src/app/user/routes/v1/index.ts
@@ -1,10 +1,16 @@
-import {findUser} from './find-user'
+import {loadRoutes} from 'utils/loader'
 import type {UserService} from 'app/user/UserService'
 import type {FastifyInstance} from 'fastify'
 
 
-export async function routes(fastify: FastifyInstance, userService: UserService, schemas: typeof import('app/user/schemas')) {
-  return Promise.all([
-    findUser(fastify, userService, schemas)
-  ])
+export async function routes(
+  fastify: FastifyInstance,
+  userService: UserService,
+  schemas: typeof import('app/user/schemas')
+) {
+  const routes = await loadRoutes<
+    {[key: string]: (fastify: FastifyInstance, service: UserService, schemas: typeof import('app/user/schemas')) => Promise<FastifyInstance>}
+    >(__dirname)
+
+  return Promise.all(routes.map(route => route(fastify, userService, schemas)))
 }
