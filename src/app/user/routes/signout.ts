87d5a667af26045f54a9ef43b7d58a6f79660b6b
QuisEgoSum <subbotin.evdokim@gmail.com>
2022-01-10 17:04:58 +0300

    feat(app/user): Added header x-localhost for signin and signout for set cookie for the localhost domain

--- src/app/user/routes/signout.ts
+++ src/app/user/routes/signout.ts
@@ -3,15 +3,27 @@ import type {UserService} from 'app/user/UserService'
 import {config} from '@config'
 
 
+interface SignoutUser {
+  Headers: {
+    'x-localhost': string
+  }
+}
+
 export async function signout(fastify: FastifyInstance, service: UserService) {
   return fastify
-    .route(
+    .route<SignoutUser>(
       {
         url: '/user/signout',
         method: 'DELETE',
         schema: {
           summary: 'User sign out',
           tags: ['User - Me'],
+          headers: {
+            'x-localhost': {
+              description: 'Any value for set cookie for the localhost domain',
+              type: 'string'
+            }
+          },
           response: {
             [200]: {
               description: 'Success message',
@@ -33,8 +45,17 @@ export async function signout(fastify: FastifyInstance, service: UserService) {
         handler: async function(request, reply) {
           await service.logout(request.session.userId, request.session.sessionId)
 
+          let cookieOptions = config.user.session.cookie
+
+          if (request.headers['x-localhost']) {
+            cookieOptions = {
+              ...cookieOptions,
+              domain: 'localhost'
+            }
+          }
+
           reply
-            .clearCookie('sessionId', config.user.session.cookie)
+            .clearCookie('sessionId', cookieOptions)
             .code(200)
             .type('application/json')
             .send({message: 'You have logged out of your account'})
