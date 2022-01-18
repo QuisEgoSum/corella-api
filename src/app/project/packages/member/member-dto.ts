a2c56e9602d2b2f0eab0cd6aa45a8ee8c9675d8b
QuisEgoSum <subbotin.evdokim@gmail.com>
2022-01-18 13:34:00 +0300

    refactor(project/member): Create MemberDto

---
+++ src/app/project/packages/member/member-dto.ts
@@ -0,0 +1,22 @@
+import {PopulatedMember} from './MemberRepository'
+import {Types} from 'mongoose'
+import {MemberStatus} from './MemberStatus'
+
+
+export class MemberDto {
+  private _id: Types.ObjectId
+  private status: MemberStatus
+  private user: {_id: Types.ObjectId; username: string; email: string | null; avatar: string}
+  private role: {_id: Types.ObjectId; name: string}
+  private createdAt: number
+  constructor(member: PopulatedMember) {
+    this._id = member._id
+    this.status = member.status
+    this.user = member.userId
+    this.role = member.roleId
+    this.createdAt = member.createdAt
+    if (this.status !== MemberStatus.PARTICIPANT) {
+      this.user.email = null
+    }
+  }
+}
